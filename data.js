29cd073cf734e97ecaec8bd61b9eeaf0366ec39e
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-09-28 19:57:55 +0530

    Add new image: lord-of-the-mysteries-screenshot-1.jpg

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 // Image data for AMV VERSE
 const images = [
   "demon-slayer-kimetsu-no-yaiba-season-2-card-1.jpg",
-  "ameku-md-doctor-detective-card.jpg"
+  "ameku-md-doctor-detective-card.jpg",
+  "lord-of-the-mysteries-screenshot-1.jpg"
 ];
