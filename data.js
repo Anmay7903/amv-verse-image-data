870b16e6343813fbea3200aa33f10a6d92658a92
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-09-28 20:00:04 +0530

    Add new image: battle-through-the-heavens-character-4.jpg

--- data.js
+++ data.js
@@ -2,5 +2,6 @@
 const images = [
   "demon-slayer-kimetsu-no-yaiba-season-2-card-1.jpg",
   "ameku-md-doctor-detective-card.jpg",
-  "lord-of-the-mysteries-screenshot-1.jpg"
+  "lord-of-the-mysteries-screenshot-1.jpg",
+  "battle-through-the-heavens-character-4.jpg"
 ];
