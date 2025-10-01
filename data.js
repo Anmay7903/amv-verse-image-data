84978c7522c65f5361c3cbee690dc152159ea1f7
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-10-01 15:30:12 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,4 @@
 // Image data for AMV VERSE
 const images = [
-  "demon-slayer-kimetsu-no-yaiba-season-2-card-1.jpg",
-  "ameku-md-doctor-detective-card.jpg",
-  "lord-of-the-mysteries-screenshot-1.jpg",
-  "battle-through-the-heavens-character-4.jpg"
+ 
 ];
