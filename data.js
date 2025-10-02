064f30a745a9b7bba4bdbbc7d3aa90b364c33d44
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-10-02 13:58:15 +0530

    Add new image: a-silent-voice-card.jpg

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 // Image data for AMV VERSE
 const images = [
   "a-couple-of-cuckoos-season-2-card.jpg",
-  "a-couple-of-cuckoos-card.jpg"
+  "a-couple-of-cuckoos-card.jpg",
+  "a-silent-voice-card.jpg"
 ];
