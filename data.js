b66c511d07b4aa3e31cde1d6376acca947fa6e4c
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-10-20 22:04:51 +0530

    Add new image: soul-land-character-Bai_Chenxiang.jpg

--- data.js
+++ data.js
@@ -75,5 +75,6 @@ const images = [
   "soul-land-character-Zhu_Zhuqing.jpg",
   "soul-land-character-Ning_Rongrong.jpg",
   "soul-land-character-Oscar.jpg",
-  "soul-land-character-Ma_Hongjun.jpg"
+  "soul-land-character-Ma_Hongjun.jpg",
+  "soul-land-character-Bai_Chenxiang.jpg"
 ];
