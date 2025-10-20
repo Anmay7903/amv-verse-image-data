d8e04f94a3948241c707027a558e6b3fb47e7bd1
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-10-20 22:12:16 +0530

    Add new image: eclipse-of-illusion-screenshot-2.jpg

--- data.js
+++ data.js
@@ -85,5 +85,6 @@ const images = [
   "eclipse-of-illusion-character-Yan_Che.jpg",
   "eclipse-of-illusion-character-Lu_Qichao.jpg",
   "eclipse-of-illusion-character-Yue_Wangshu.jpg",
-  "eclipse-of-illusion-screenshot-1.jpg"
+  "eclipse-of-illusion-screenshot-1.jpg",
+  "eclipse-of-illusion-screenshot-2.jpg"
 ];
