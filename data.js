9f6decc223fda3ffcb97b7ce9adfd1765c845ce9
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-10-20 22:02:02 +0530

    Add new image: soul-land-character-Dai_Mubai.jpg

--- data.js
+++ data.js
@@ -70,5 +70,6 @@ const images = [
   "soul-land-2-screenshot-3.jpg",
   "soul-land-2-screenshot-4.jpg",
   "soul-land-character-Tang_San.jpg",
-  "soul-land-character-Xiao_Wu.jpg"
+  "soul-land-character-Xiao_Wu.jpg",
+  "soul-land-character-Dai_Mubai.jpg"
 ];
