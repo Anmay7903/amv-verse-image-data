c56d0b7f67b7f4cb0b3cbe0c097b09c3a483d94e
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-10-20 21:51:05 +0530

    Add new image: soul-land-2-screenshot-2.jpg

--- data.js
+++ data.js
@@ -65,5 +65,6 @@ const images = [
   "soul-land-2-character-Ice_Empress.jpg",
   "soul-land-2-character-Snow_Emperor.jpg",
   "soul-land-2-character-Bei_Bei.jpg",
-  "soul-land-2-screenshot-1.jpg"
+  "soul-land-2-screenshot-1.jpg",
+  "soul-land-2-screenshot-2.jpg"
 ];
