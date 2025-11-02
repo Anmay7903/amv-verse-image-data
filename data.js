6c6d7dde009d4b957137b35ee1c51d7a2e7ed733
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-11-02 20:26:16 +0530

    Add new image: blue-lock-movie-episode-nagi-screenshot-7.jpg

--- data.js
+++ data.js
@@ -138,5 +138,6 @@ const images = [
   "blue-lock-movie-episode-nagi-screenshot-4.jpg",
   "blue-lock-movie-episode-nagi-screenshot.jpg",
   "blue-lock-movie-episode-nagi-screenshot-5.jpg",
-  "blue-lock-movie-episode-nagi-screenshot-6.jpg"
+  "blue-lock-movie-episode-nagi-screenshot-6.jpg",
+  "blue-lock-movie-episode-nagi-screenshot-7.jpg"
 ];
