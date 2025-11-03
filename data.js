65008a8ea477a4285df0d6dd77d65cf2f37912ec
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-11-03 09:59:29 +0530

    Add new image: demon-slayer-kimetsu-no-yaiba-season-1-screenshot-5.jpg

--- data.js
+++ data.js
@@ -170,5 +170,6 @@ const images = [
   "demon-slayer-kimetsu-no-yaiba-season-1-screenshot-1.jpg",
   "demon-slayer-kimetsu-no-yaiba-season-1-screenshot-2.jpg",
   "demon-slayer-kimetsu-no-yaiba-season-1-screenshot-3.jpg",
-  "demon-slayer-kimetsu-no-yaiba-season-1-screenshot-4.jpg"
+  "demon-slayer-kimetsu-no-yaiba-season-1-screenshot-4.jpg",
+  "demon-slayer-kimetsu-no-yaiba-season-1-screenshot-5.jpg"
 ];
