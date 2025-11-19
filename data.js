eb7398030cd96a646a2f12ffbe5e0c16b9fdf13a
Anmay7903 <220342388+Anmay7903@users.noreply.github.com>
2025-11-19 22:33:41 +0530

    Update images list - added 196 images

--- data.js
+++ data.js
@@ -192,5 +192,8 @@ const images = [
   "new-saga-screenshot-4.jpg",
   "new-saga-screenshot-5.jpg",
   "new-saga-screenshot-6.jpg",
-  "battle-through-the-heavens-character-xiao_yan.jpg"
+  "battle-through-the-heavens-character-xiao_yan.jpg",
+  "jujutsu-kaisen-screenshot-1",
+  "jujutsu-kaisen-screenshot-2",
+  "jujutsu-kaisen-screenshot-3"
 ];
